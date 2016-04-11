6b560ab1ce3bf186beb96444de62032e90b80c36
qyl <448979688@qq.com>
2016-04-11 12:47:00 +0800

    添加了滑动播放,滑动修改音量和屏幕亮度,点击Slider切换播放进度

--- WMPlayer/WMPlayer/WMPlayer.m
+++ WMPlayer/WMPlayer/WMPlayer.m
@@ -17,16 +17,21 @@
 #import "WMPlayer.h"
 #define WMVideoSrcName(file) [@"WMPlayer.bundle" stringByAppendingPathComponent:file]
 #define WMVideoFrameworkSrcName(file) [@"Frameworks/WMPlayer.framework/WMPlayer.bundle" stringByAppendingPathComponent:file]
+#define kHalfWidth self.frame.size.width * 0.5
+#define kHalfHeight self.frame.size.height * 0.5
 
 
 static void *PlayViewCMTimeValue = &PlayViewCMTimeValue;
 
 static void *PlayViewStatusObservationContext = &PlayViewStatusObservationContext;
 
-@interface WMPlayer ()
+@interface WMPlayer () <UIGestureRecognizerDelegate>
 @property (nonatomic,assign)CGPoint firstPoint;
 @property (nonatomic,assign)CGPoint secondPoint;
 @property (nonatomic, retain)NSDateFormatter *dateFormatter;
+//视频进度条的单击事件
+@property (nonatomic, strong) UITapGestureRecognizer *tap;
+@property (nonatomic, assign) CGPoint originalPoint;
 @end
 
 
@@ -88,10 +93,21 @@ static void *PlayViewStatusObservationContext = &PlayViewStatusObservationContex
             
         }];
         
+        //创建亮度的进度条
+        self.lightSlider = [[UISlider alloc]initWithFrame:CGRectMake(0, 0, 0, 0)];
+        self.lightSlider.hidden = YES;
+        self.lightSlider.minimumValue = 0;
+        self.lightSlider.maximumValue = 1;
+        //        进度条的值等于当前系统亮度的值,范围都是0~1
+        self.lightSlider.value = [UIScreen mainScreen].brightness;
+        [self.lightSlider addTarget:self action:@selector(updateLightValue:) forControlEvents:UIControlEventValueChanged];
+        [self addSubview:self.lightSlider];
+
         
         
         MPVolumeView *volumeView = [[MPVolumeView alloc]init];
         [self addSubview:volumeView];
+        volumeView.frame = CGRectMake(-1000, -100, 100, 100);
         [volumeView sizeToFit];
         
         
@@ -106,7 +122,7 @@ static void *PlayViewStatusObservationContext = &PlayViewStatusObservationContex
         systemSlider.autoresizesSubviews = NO;
         systemSlider.autoresizingMask = UIViewAutoresizingNone;
         [self addSubview:systemSlider];
-        systemSlider.hidden = YES;
+//        systemSlider.hidden = YES;
         
         
         
@@ -127,6 +143,13 @@ static void *PlayViewStatusObservationContext = &PlayViewStatusObservationContex
         self.progressSlider.minimumTrackTintColor = [UIColor greenColor];
         self.progressSlider.value = 0.0;//指定初始值
         [self.progressSlider addTarget:self action:@selector(updateProgress:) forControlEvents:UIControlEventTouchUpInside];
+        
+        //给进度条添加单击手势
+        self.tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(actionTapGesture:)];
+        self.tap.delegate = self;
+        [self.progressSlider addGestureRecognizer:self.tap];
+        [self.bottomView addSubview:self.progressSlider];
+        
         [self.bottomView addSubview:self.progressSlider];
         
         //autoLayout slider
@@ -221,6 +244,16 @@ static void *PlayViewStatusObservationContext = &PlayViewStatusObservationContex
     }
     return self;
 }
+
+//视频进度条的点击事件
+- (void)actionTapGesture:(UITapGestureRecognizer *)sender {
+    
+    CGPoint touchPoint = [sender locationInView:self.progressSlider];
+    CGFloat value = (self.progressSlider.maximumValue - self.progressSlider.minimumValue) * (touchPoint.x / self.progressSlider.frame.size.width );
+    [self.progressSlider setValue:value animated:YES];
+    [self.player seekToTime:CMTimeMakeWithSeconds(self.progressSlider.value, 1)];
+}
+
 - (void)updateSystemVolumeValue:(UISlider *)slider{
     systemSlider.value = slider.value;
 }
@@ -263,13 +296,15 @@ static void *PlayViewStatusObservationContext = &PlayViewStatusObservationContex
         self.durationTimer = [NSTimer timerWithTimeInterval:0.2 target:self selector:@selector(finishedPlay:) userInfo:nil repeats:YES];
         [[NSRunLoop currentRunLoop] addTimer:self.durationTimer forMode:NSDefaultRunLoopMode];
     }
-    sender.selected = !sender.selected;
+//    sender.selected = !sender.selected;
     if (self.player.rate != 1.f) {
         if ([self currentTime] == [self duration])
             [self setCurrentTime:0.f];
         [self.player play];
+        sender.selected = NO;
     } else {
         [self.player pause];
+        sender.selected = YES;
     }
     
     //    CMTime time = [self.player currentTime];
@@ -307,13 +342,15 @@ static void *PlayViewStatusObservationContext = &PlayViewStatusObservationContex
 #pragma mark
 #pragma mark - 双击手势方法
 - (void)handleDoubleTap{
-    self.playOrPauseBtn.selected = !self.playOrPauseBtn.selected;
+//    self.playOrPauseBtn.selected = !self.playOrPauseBtn.selected;
     if (self.player.rate != 1.f) {
         if ([self currentTime] == self.duration)
             [self setCurrentTime:0.f];
         [self.player play];
+        self.playOrPauseBtn.selected = NO;
     } else {
         [self.player pause];
+        self.playOrPauseBtn.selected = YES;
     }
 }
 
@@ -524,18 +561,79 @@ static void *PlayViewStatusObservationContext = &PlayViewStatusObservationContex
         self.firstPoint = [touch locationInView:self];
         
     }
-    UISlider *volumeSlider = (UISlider *)[self viewWithTag:1000];
-    volumeSlider.value = systemSlider.value;
+    self.volumeSlider.value = systemSlider.value;
+    //记录下第一个点的位置,用于moved方法判断用户是调节音量还是调节视频
+    self.originalPoint = self.firstPoint;
+    
+    
+//    UISlider *volumeSlider = (UISlider *)[self viewWithTag:1000];
+//    volumeSlider.value = systemSlider.value;
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
     for(UITouch *touch in event.allTouches) {
         self.secondPoint = [touch locationInView:self];
     }
-    systemSlider.value += (self.firstPoint.y - self.secondPoint.y)/500.0;
-    UISlider *volumeSlider = (UISlider *)[self viewWithTag:1000];
-    volumeSlider.value = systemSlider.value;
+    
+    //判断是左右滑动还是上下滑动
+    CGFloat verValue =fabs(self.originalPoint.y - self.secondPoint.y);
+    CGFloat horValue = fabs(self.originalPoint.x - self.secondPoint.x);
+    
+    //如果竖直方向的偏移量大于水平方向的偏移量,那么是调节音量或者亮度
+    if (verValue > horValue) {//上下滑动
+        
+        //判断是全屏模式还是正常模式
+        if (self.isFullscreen) {//全屏下
+            
+            //判断刚开始的点是左边还是右边,左边控制音量
+            if (self.originalPoint.x <= kHalfHeight) {//全屏下:point在view的左边(控制音量)
+                
+                /* 手指上下移动的计算方式,根据y值,刚开始进度条在0位置,当手指向上移动600个点后,当手指向上移动N个点的距离后,
+                 当前的进度条的值就是N/600,600随开发者任意调整,数值越大,那么进度条到大1这个峰值需要移动的距离也变大,反之越小 */
+                systemSlider.value += (self.firstPoint.y - self.secondPoint.y)/600.0;
+                self.volumeSlider.value = systemSlider.value;
+            }else{//全屏下:point在view的右边(控制亮度)
+                //右边调节屏幕亮度
+                self.lightSlider.value += (self.firstPoint.y - self.secondPoint.y)/600.0;
+                [[UIScreen mainScreen] setBrightness:self.lightSlider.value];
+                
+            }
+        }else{//非全屏
+            
+            //判断刚开始的点是左边还是右边,左边控制音量
+            if (self.originalPoint.x <= kHalfWidth) {//非全屏下:point在view的左边(控制音量)
+                
+                /* 手指上下移动的计算方式,根据y值,刚开始进度条在0位置,当手指向上移动600个点后,当手指向上移动N个点的距离后,
+                 当前的进度条的值就是N/600,600随开发者任意调整,数值越大,那么进度条到大1这个峰值需要移动的距离也变大,反之越小 */
+                systemSlider.value += (self.firstPoint.y - self.secondPoint.y)/600.0;
+                self.volumeSlider.value = systemSlider.value;
+            }else{//非全屏下:point在view的右边(控制亮度)
+                //右边调节屏幕亮度
+                self.lightSlider.value += (self.firstPoint.y - self.secondPoint.y)/600.0;
+                [[UIScreen mainScreen] setBrightness:self.lightSlider.value];
+                
+            }
+        }
+    }else{//左右滑动,调节视频的播放进度
+        //视频进度不需要除以600是因为self.progressSlider没设置最大值,它的最大值随着视频大小而变化
+        //要注意的是,视频的一秒时长相当于progressSlider.value的1,视频有多少秒,progressSlider的最大值就是多少
+        self.progressSlider.value -= (self.firstPoint.x - self.secondPoint.x);
+        
+        [self.player seekToTime:CMTimeMakeWithSeconds(self.progressSlider.value, 1)];
+        
+        //滑动太快可能会停止播放,所以这里自动继续播放
+        [self.player play];
+        self.playOrPauseBtn.selected = NO;
+    }
+    
     self.firstPoint = self.secondPoint;
+    
+    
+    
+//    systemSlider.value += (self.firstPoint.y - self.secondPoint.y)/500.0;
+//    UISlider *volumeSlider = (UISlider *)[self viewWithTag:1000];
+//    volumeSlider.value = systemSlider.value;
+//    self.firstPoint = self.secondPoint;
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
