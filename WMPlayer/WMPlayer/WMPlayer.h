06033b10a0688f9b032a5a1eb27c47eb34f6564f
zhengwenming <740747055@qq.com>
2016-04-11 13:43:32 +0800

    fix bugs

--- WMPlayer/WMPlayer/WMPlayer.h
+++ WMPlayer/WMPlayer/WMPlayer.h
@@ -51,8 +51,7 @@
 @property(nonatomic,retain)UISlider *progressSlider;
 @property(nonatomic,retain)UISlider *volumeSlider;
 @property(nonatomic,copy) NSString *videoURLStr;
-/** 亮度的进度条 */
-@property (nonatomic, retain) UISlider *lightSlider;
+
 /**
  *  定时器
  */
