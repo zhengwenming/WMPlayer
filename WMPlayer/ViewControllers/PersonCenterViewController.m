215ac355b072eca09f1f3435fc97309813d51ddb
zhengwenming <740747055@qq.com>
2016-12-02 11:25:36 +0800

    zhushi

--- WMPlayer/ViewControllers/PersonCenterViewController.m
+++ WMPlayer/ViewControllers/PersonCenterViewController.m
@@ -8,7 +8,7 @@
 //
 
 #import "PersonCenterViewController.h"
-#import "TestViewController.h"
+//#import "TestViewController.h"
 #import <UIKit/UIKit.h>
 
 @implementation PersonCenterViewController
@@ -16,21 +16,21 @@
 {
     self.view.backgroundColor = [UIColor  whiteColor];
 
-    UIButton *palyBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    palyBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    palyBtn.backgroundColor = [UIColor redColor];
-    [palyBtn setTitle:@"记忆播放" forState:UIControlStateNormal];
-    [palyBtn setTitle:@"记忆播放" forState:UIControlStateSelected];
-    [palyBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    palyBtn.titleLabel.font = [UIFont systemFontOfSize:15.0];
-    [palyBtn setImage:[UIImage imageNamed:@"commentBtn"] forState:UIControlStateNormal];
-    [palyBtn setImage:[UIImage imageNamed:@"commentBtn"] forState:UIControlStateSelected];
-    palyBtn.frame = CGRectMake(0, 0, 200, 50);
-    [palyBtn addTarget:self action:@selector(testSeekToPlay:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:palyBtn];
-    palyBtn.center = self.view.center;
+//    UIButton *palyBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+//    palyBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+//    palyBtn.backgroundColor = [UIColor redColor];
+//    [palyBtn setTitle:@"记忆播放" forState:UIControlStateNormal];
+//    [palyBtn setTitle:@"记忆播放" forState:UIControlStateSelected];
+//    [palyBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+//    palyBtn.titleLabel.font = [UIFont systemFontOfSize:15.0];
+//    [palyBtn setImage:[UIImage imageNamed:@"commentBtn"] forState:UIControlStateNormal];
+//    [palyBtn setImage:[UIImage imageNamed:@"commentBtn"] forState:UIControlStateSelected];
+//    palyBtn.frame = CGRectMake(0, 0, 200, 50);
+//    [palyBtn addTarget:self action:@selector(testSeekToPlay:) forControlEvents:UIControlEventTouchUpInside];
+//    [self.view addSubview:palyBtn];
+//    palyBtn.center = self.view.center;
 }
--(void)testSeekToPlay:(UIButton *)sender{
-    [self.navigationController pushViewController:[[TestViewController alloc] init] animated:YES];
-}
+//-(void)testSeekToPlay:(UIButton *)sender{
+////    [self.navigationController pushViewController:[[TestViewController alloc] init] animated:YES];
+//}
 @end
