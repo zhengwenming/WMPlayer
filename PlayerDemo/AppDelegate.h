42109b47414d07f0c2b0dfc8b037ff13eae8c456
DS-Team <513878863@qq.com>
2016-12-05 09:41:30 +0800

    pod 上架1

---
+++ PlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayerDemo
+//
+//  Created by 博爱 on 2016/12/5.
+//  Copyright © 2016年 DS-Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
